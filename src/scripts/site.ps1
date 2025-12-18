1afd95a6182a4f2926dce34c10507a657ae95adf
Fabricio Nascimento <fabriciotn82@gmail.com>
2025-12-17 23:47:20 -0300

    feat: ajuste no link do gitlab

--- src/scripts/site.ps1
+++ src/scripts/site.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/lmlpuc/lmlarm003/refs/heads/main/src/index.html' -OutFile 'C:\inetpub\wwwroot\index.html'
+Invoke-WebRequest -Uri 'https://github.com/2025-11-PUC-MBA-Cloud-G1-GIT/trabalho-devops-g1/tree/main/src/index.html' -OutFile 'C:\inetpub\wwwroot\index.html'
